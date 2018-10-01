33faf93297642a2b6cc4f453ccee343d8a1c4ac4
wivj4zm4fpg0 <nzswlrkad8cw@gmail.com>
2018-10-01 14:05:33 +0900

    code format

--- test.py
+++ test.py
@@ -39,7 +39,6 @@ def test(data_loader, model, opt, class_names):
         for i, (inputs, targets) in enumerate(data_loader):
             data_time.update(time.time() - end_time)
 
-            # inputs = Variable(inputs, volatile=True)
             outputs = model(inputs)
             if not opt.no_softmax_in_test:
                 outputs = F.softmax(outputs)
@@ -63,11 +62,9 @@ def test(data_loader, model, opt, class_names):
 
             print('[{}/{}]\t'
                   'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
-                  'Data {data_time.val:.3f} ({data_time.avg:.3f})\t'.format(
-                i + 1,
-                len(data_loader),
-                batch_time=batch_time,
-                data_time=data_time))
+                  'Data {data_time.val:.3f} ({data_time.avg:.3f})\t'.format(i + 1, len(data_loader),
+                                                                            batch_time=batch_time,
+                                                                            data_time=data_time))
     with open(
             os.path.join(opt.result_path, '{}.json'.format(opt.test_subset)),
             'w') as f:
--- validation.py
+++ validation.py
@@ -40,15 +40,12 @@ def val_epoch(epoch, data_loader, model, criterion, opt, logger):
                   'Data {data_time.val:.3f} ({data_time.avg:.3f})\t'
                   'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
                   'Acc-Top1 {acc.val:.3f} ({acc.avg:.3f})\t'
-                  'Acc-Top5 {acc5.val:.3f} ({acc5.avg:.3f})'.format(
-                epoch,
-                i + 1,
-                len(data_loader),
-                batch_time=batch_time,
-                data_time=data_time,
-                loss=losses,
-                acc=accuracies,
-                acc5=accuracies5))
+                  'Acc-Top5 {acc5.val:.3f} ({acc5.avg:.3f})'.format(epoch, i + 1, len(data_loader),
+                                                                    batch_time=batch_time,
+                                                                    data_time=data_time,
+                                                                    loss=losses,
+                                                                    acc=accuracies,
+                                                                    acc5=accuracies5))
 
     epoch_time = time.time() - epoch_time
 
